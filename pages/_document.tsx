32a3ef3fe1bdcced957dbeaefe888c8028a3effc
Owen Roth <rothowen27@gmail.com>
2022-08-29 16:33:04 -0500

    added gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,38 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-<       link href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Baskervville:ital@0;1&display=swap" rel="stylesheet" />
-        </Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Baskervville:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
+        <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GTM-5MVWS5V"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GTM-5MVWS5V');
+        `}
+      </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
