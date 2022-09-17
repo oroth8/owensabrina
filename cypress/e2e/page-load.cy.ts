85984ab179ad3cb4750a7ea1d9516faa7ab0d090
Owen Roth <rothowen27@gmail.com>
2022-09-17 13:44:36 -0500

    added tests

---
+++ cypress/e2e/page-load.cy.ts
@@ -0,0 +1,37 @@
+import { Utility } from '../support/utility';
+
+const url: string = new Utility().getBaseUrl() || 'http://localhost:3000';
+
+const pageURL = (path: string) => {
+  return `${url}${path}`;
+}
+
+describe('Verify Page Loads', () => {
+    it('Verify Tavel Page', () => {
+        cy.visit(pageURL('/travel'));
+    });
+    it('Verify Wedding Party Page', () => {
+        cy.visit(pageURL('/wedding-party'));
+    });
+    it('Verify RSVP Page', () => {
+        cy.visit(pageURL('/RSVP'));
+    });
+    it('Verify Registry Page', () => {
+        cy.visit(pageURL('/registry'));
+    });
+    it('Verify Events Page', () => {
+        cy.visit(pageURL('/events'));
+    });
+    it('Verify Address Page', () => {
+        cy.visit(pageURL('/address'));
+    });
+    it('Verify Engagement Party RSVP Page', () => {
+        cy.visit(pageURL('/engagement_party/rsvp'));
+    });
+    it('Verify Engagement Party Find Page', () => {
+        cy.visit(pageURL('/engagement_party/rsvp'));
+    });
+    it('Verify Engagement Party Edit Page', () => {
+        cy.visit(pageURL('/engagement_party/edit/1'));
+    });
+  });
---
+++ cypress/support/utility.ts
@@ -0,0 +1,12 @@
+export class Utility {
+    getBaseUrl() {
+        let envi = Cypress.env('ENV'); //Get the value of evnironment variable i.e ENV
+        let vercel_url = process.env.VERCEL_URL;
+        if (envi == 'production') //Check the value
+            return "https://www.proudction-website.com"; //return desired url
+        else if (envi == 'development')
+            return vercel_url;
+        else if (envi == 'local')
+            return "http://localhost:3000";
+    }
+}
