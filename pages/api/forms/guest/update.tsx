d079ba17fdd82a05a0702dc738fc2d240174ebf3
Owen Roth <rothowen27@gmail.com>
2022-10-02 17:59:56 -0500

    added update functionality to show form

---
+++ pages/api/forms/guest/update.tsx
@@ -0,0 +1,84 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { withSentry } from "@sentry/nextjs";
+
+async function userHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "PUT") {
+    return res.status(405).json({ error: "only PUT requests allowed" });
+  }
+  const { name, email, phone, address, address2, city, state, postal, id } = req.body;
+  
+  try {
+    const result = await makeRequest( name, email, phone, address, address2, city, state, postal, id);
+    if ("error" in result) {
+      res.status(500).send(result);
+    } else {
+      res.status(200).send(result);
+    }
+  } catch (err) {
+    res.status(500).send({ error: "failed to fetch data" });
+  }
+}
+
+type Response = {
+  id: number;
+  name: string;
+  email: string;
+  address: string;
+  address2: string;
+  city: string;
+  state: string;
+  postal: string;
+  created_at: string;
+  updated_at: string;
+  phone: string;
+};
+
+type Error = {
+  error: string;
+};
+
+async function makeRequest(
+  name: string,
+  email: string,
+  phone: string,
+  address: string,
+  address2: string,
+  city: string,
+  state: string,
+  postal: string,
+  id: string,
+): Promise<Response | Error> {
+  const data = {
+    name,
+    email,
+    address,
+    address2,
+    city,
+    state,
+    postal,
+    phone: phone.replace(/\D+/g, ""),
+    id
+  };
+
+  const JSONdata = JSON.stringify(data);
+  const endpoint = `${process.env.API_URL}/api/v1/guests/${id}`;
+
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.API_KEY}`,
+    },
+    body: JSONdata,
+  };
+
+  const response = await fetch(endpoint, options);
+
+  const result = await response.json();
+  return result;
+}
+
+export default withSentry(userHandler);
--- pages/invite-address/update/[id].tsx
+++ pages/invite-address/update/[id].tsx
@@ -12,123 +12,147 @@ import capitalize from "../../../helpers/capitalize";
 import { useRouter } from "next/router";
 
 const Page: NextPageWithLayout = () => {
-    type formDataType = {[key:string]: FormDataEntryValue}
-    const responseBody: formDataType = {}
+  type formDataType = { [key: string]: FormDataEntryValue };
 
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState("")
-    const [success, setSuccess] = useState("")
-    // FORM INPUT STATES
-const [firstName, setFirstName] = useState("")
-const [lastName, setLastName] = useState("")
-    const [email, setEmail] = useState("");
-    const [phoneValue, setPhoneValue] = useState("");
-    const [address, setAddress] = useState("");
-    const [address2, setAddress2] = useState("");
-    const [city, setCity] = useState("");
-    const [state, setState] = useState("");
-    const [postal, setPostal] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  // FORM INPUT STATES
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneValue, setPhoneValue] = useState("");
+  const [address, setAddress] = useState("");
+  const [address2, setAddress2] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [postal, setPostal] = useState("");
 
-    const handleNameRes = (name: string) => {
-        const splitName = name.split(' ')
-        setFirstName(splitName[0])
-        setLastName(splitName[1])
+  const handleNameRes = (name: string) => {
+    const splitName = name.split(" ");
+    setFirstName(splitName[0]);
+    setLastName(splitName[1]);
+  };
+
+  const router = useRouter();
+  const { id } = router.query;
+
+
+
+  const processId = (id: any) => {
+    if (typeof id === "undefined") {
+      return "0"
+    } else if (Array.isArray(id) ) {
+      return "0"
+    } else if (!id ) {
+      return "0"
+    }else{
+      return id
     }
+   
+  };
 
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const formattedPhoneNumber = PhoneInput(e.target.value);
+    setPhoneValue(formattedPhoneNumber);
+  };
 
-    const router = useRouter();
-    const { id } = router.query;
+  const getGuestDetails = async (id: string | string[]) => {
+    const endpoint = `/api/forms/guest/${id}`;
+    const options = { method: "GET" };
 
-
-    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-        const formattedPhoneNumber = PhoneInput(e.target.value);
-        setPhoneValue(formattedPhoneNumber);
-      };
-
-
-
-    const getGuestDetails = async (id: string | string[]) => {
-        const endpoint = `/api/forms/guest/${id}`;
-        const options = { method: "GET" };
-    
-        try {
-          const response = await fetch(endpoint, options);
-          const result = await response.json();
-          if ("error" in result) {
-            setLoading(false);
-            setError(result.error);
-          } else if ("id" in result) {
-            setLoading(false);
-            handleNameRes(capitalize(result.name));
-            setEmail(result.email);
-            setPhoneValue(PhoneInput(result.phone));
-            setAddress(result.address);
-            setAddress2(result.address2);
-            setCity(result.city)
-            setState(result.state)
-            setPostal(result.postal)
-          }
-        } catch (e: any) {
-          setLoading(false);
-          setError(e.message);
-        }
-      };
-    
-
-    const inputChangeHandler = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget as HTMLFormElement)
-        formData.forEach((value, property:string) => responseBody[property] = value);
-        const name = `${responseBody["firstName"]} ${responseBody["lastName"]}`
-        delete responseBody["firstName"]
-        delete responseBody["lastName"]
-        Object.assign(responseBody, {name})
-
-        const JSONdata = JSON.stringify(responseBody)
-
-        const endpoint = "/api/forms/guest/create"
-
-        const options = {
-                  method: 'POST',
-                     headers: {
-                    'Content-Type': 'application/json',
-                  },
-                  body: JSONdata,
-                }
-        setLoading(true)
-        const response = await fetch(endpoint, options)
-        const result = await response.json()
-        if("error" in result){
-            setLoading(false)
-            setError(result.error)
-        }else if("name" in result){
-            setLoading(false)
-            window.location.href = "/invite-address/thank-you"
-            // setSuccess("Thank you, your response has been saved. We look forward to having you at our wedding!")
-        }else{
-            setLoading(false)
-            setError("Error please contact us by email and we will make sure you are added to the list!")
-        }
+    try {
+      const response = await fetch(endpoint, options);
+      const result = await response.json();
+      if ("error" in result) {
+        setLoading(false);
+        setError(result.error);
+      } else if ("id" in result) {
+        setLoading(false);
+        handleNameRes(capitalize(result.name));
+        setEmail(result.email);
+        setPhoneValue(PhoneInput(result.phone));
+        setAddress(result.address);
+        setAddress2(result.address2);
+        setCity(result.city);
+        setState(result.state);
+        setPostal(result.postal);
+      }
+    } catch (e: any) {
+      setLoading(false);
+      setError(e.message);
     }
+  };
 
-    useEffect(() => {
-        if(id){
-            getGuestDetails(id)
-        }},[id])
-        
+  const submitHandler = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    setLoading(true);
+    const responseBody: formDataType = {
+      name: `${firstName} ${lastName}`,
+      email,
+      phone: phoneValue,
+      address,
+      address2,
+      city,
+      state,
+      postal,
+      id: processId(id),
+    };
+
+    const JSONdata = JSON.stringify(responseBody);
+
+    const endpoint = "/api/forms/guest/update";
+
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
+    const response = await fetch(endpoint, options);
+    const result = await response.json();
+    if ("error" in result) {
+      setLoading(false);
+      setError(result.error);
+    } else if ("name" in result) {
+      setLoading(false);
+      window.location.href = "/invite-address/thank-you";
+      // setSuccess("Thank you, your response has been saved. We look forward to having you at our wedding!")
+    } else {
+      setLoading(false);
+      setError(
+        "Error please contact us by email and we will make sure you are added to the list!"
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      getGuestDetails(id);
+    }
+  }, [id]);
+
   return (
     <>
-    <Tags title={"Address Confirmation"} description={"Confirm your address to receive a wedding invitation."} />
-    <Nav/>
-    <div className="pt-8 mx-auto max-w-2xl font-display text-green-primary">
-        {error && (
-            <Alert message={error} />
-        )}
-        {success && <Success message={success}/>}
-        <form onSubmit={inputChangeHandler}>
+      <Tags
+        title={"Address Confirmation"}
+        description={"Confirm your address to receive a wedding invitation."}
+      />
+      <Nav />
+      <div className="pt-8 mx-auto max-w-2xl font-display text-green-primary">
+        {error && <Alert message={error} />}
+        {success && <Success message={success} />}
+        <form onSubmit={(e)=> submitHandler(e)}>
           <div>
-            <h3 className="text-lg font-medium leading-6">Contact Information</h3>
-            <p className="mt-1 text-sm">Use a permanent address where you can receive mail.</p>
+            <h3 className="text-lg font-medium leading-6">
+              Contact Information
+            </h3>
+            <p className="mt-1 text-sm">
+              Use a permanent address where you can receive mail.
+            </p>
           </div>
           <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
             <div className="sm:col-span-3">
@@ -140,7 +164,7 @@ const [lastName, setLastName] = useState("")
                   type="text"
                   name="firstName"
                   onChange={(e) => setFirstName(e.target.value)}
-                    value={firstName}
+                  value={firstName}
                   id="firstName"
                   autoComplete="given-name"
                   required
@@ -158,7 +182,7 @@ const [lastName, setLastName] = useState("")
                   type="text"
                   name="lastName"
                   onChange={(e) => setLastName(e.target.value)}
-                    value={lastName}
+                  value={lastName}
                   id="lastName"
                   autoComplete="family-name"
                   required
@@ -200,7 +224,6 @@ const [lastName, setLastName] = useState("")
                   maxLength={14}
                   minLength={14}
                   title="numbers only, no special chars."
-
                   className="block p-2 w-full rounded border-gray-300 border shadow-sm focus:outline-none focus:border-green-dark focus:ring-green-dark sm:text-sm"
                 />
               </div>
@@ -215,7 +238,7 @@ const [lastName, setLastName] = useState("")
                   type="text"
                   name="address"
                   onChange={(e) => setAddress(e.target.value)}
-                    value={address}
+                  value={address}
                   id="address"
                   autoComplete="address-line1"
                   required
@@ -233,7 +256,7 @@ const [lastName, setLastName] = useState("")
                   type="text"
                   name="address2"
                   onChange={(e) => setAddress2(e.target.value)}
-                    value={address2}
+                  value={address2}
                   id="address2"
                   autoComplete="address-line2"
                   className="block p-2 w-full rounded border-gray-300 border shadow-sm focus:outline-none focus:border-green-dark focus:ring-green-dark sm:text-sm"
@@ -249,8 +272,8 @@ const [lastName, setLastName] = useState("")
                 <input
                   type="text"
                   name="city"
-                    onChange={(e) => setCity(e.target.value)}
-                    value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  value={city}
                   id="city"
                   autoComplete="address-level2"
                   className="block p-2 w-full rounded border-gray-300 border shadow-sm focus:outline-none focus:border-green-dark focus:ring-green-dark sm:text-sm"
@@ -268,7 +291,7 @@ const [lastName, setLastName] = useState("")
                   name="state"
                   id="state"
                   onChange={(e) => setState(e.target.value)}
-                    value={state}
+                  value={state}
                   autoComplete="address-level1"
                   required
                   maxLength={2}
@@ -295,18 +318,27 @@ const [lastName, setLastName] = useState("")
                 />
               </div>
             </div>
-            
+
             <div className="hidden sm:block sm:col-span-2"></div>
             <div className="hidden sm:block sm:col-span-2"></div>
             {loading ? (
-                <Image src="/images/loading.gif" height={100} width={100} alt={"Loading"} />
-
-            ):(<button type="submit" className="bg-green-primary text-white rounded text-display uppercase p-2 sm:col-span-2">Submit</button>)}
-           
+              <Image
+                src="/images/loading.gif"
+                height={100}
+                width={100}
+                alt={"Loading"}
+              />
+            ) : (
+              <button
+                type="submit"
+                className="bg-green-primary text-white rounded text-display uppercase p-2 sm:col-span-2"
+              >
+                Update
+              </button>
+            )}
           </div>
-          </form>
-        </div>
-   
+        </form>
+      </div>
     </>
   );
 };
