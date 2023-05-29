0322b473f0af961d9b1cc5e3c2d3887020dd754a
Owen Roth <rothowen27@gmail.com>
2023-05-29 09:42:08 -0500

    add phone and name lookup pages

--- pages/rsvp/phone.tsx
+++ pages/rsvp/phone.tsx
@@ -1,11 +1,19 @@
-import type { ReactElement } from "react";
+import type { ReactElement, ChangeEvent } from "react";
+import { useState } from "react";
 import Layout from "../../components/nav/Layout";
 import type { NextPageWithLayout } from ".././_app";
 import Nav from "../../components/nav/Nav";
 import Tags from "../../components/Tags";
 import Link from "next/link";
+import PhoneInput from "../../components/PhoneInput";
 
 const Page: NextPageWithLayout = () => {
+  const [phoneValue, setPhoneValue] = useState("");
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const formattedPhoneNumber = PhoneInput(e.target.value);
+    setPhoneValue(formattedPhoneNumber);
+  };
   return (
     <>
       <Tags
@@ -18,26 +26,32 @@ const Page: NextPageWithLayout = () => {
       <div className="text-green-primary uppercase text-center font-display py-10 sm:py-20 tracking-widest px-10">
         <h2 className="text-3xl">YOU&apos;RE INVITED</h2>
         <div className="bg-green-primary h-1 w-48 my-8 sm:my-16 mx-auto"></div>
-        <p className="text-xl sm:text-2xl">Enter your displayed name from the invitation</p>
+        <p className="text-xl sm:text-2xl">
+          Enter the phone number you provided
+        </p>
         <div className="mt-4">
-          <label htmlFor="name" className="sr-only">
-            Name
+          <label htmlFor="phone" className="sr-only">
+            Phone Number
           </label>
           <input
             type="text"
-            name="name"
-            id="name"
+            name="phone"
+            id="phone"
+            onChange={(e) => handleInput(e)}
+            value={phoneValue}
             className="block w-full p-2 mx-auto max-w-xl border-gray border rounded h-10 shadow-sm focus:border-green-primary focus:ring-green-primary sm:text-sm"
-            placeholder="John Smith"
-            autoComplete="name"
+            placeholder="Enter Phone Number Here"
+            autoComplete="tel"
+            maxLength={14}
+            minLength={14}
             required
           />
         </div>
         <button className="bg-green-primary text-white w-full mx-auto max-w-xl mt-4 p-4 font-display tracking-widest">
           FIND RSVP
         </button>
-        <Link href="/rsvp/phone" className="block mt-4">
-        <small>Try phone number instead</small>
+        <Link href="/rsvp" className="block mt-4">
+          <small>Try invitation name instead</small>
         </Link>
       </div>
     </>
