49234ff944c21bd95ef2f1c22551f062fded3a71
Owen Roth <69156111+oroth8@users.noreply.github.com>
2023-05-29 09:11:55 -0500

    Event page updates (#17)
    * update event page with event times
    
    * change transportation copy
    
    * more copy changes

--- pages/events.tsx
+++ pages/events.tsx
@@ -3,6 +3,7 @@ import Layout from "../components/nav/Layout";
 import type { NextPageWithLayout } from "./_app";
 import Nav from "../components/nav/Nav";
 import Tags from "../components/Tags";
+import Link from "next/link";
 
 const Page: NextPageWithLayout = () => {
   return (
@@ -20,9 +21,24 @@ const Page: NextPageWithLayout = () => {
             <h2 className="text-4xl font-bold tracking-tight text-white sm:text-3xl lg:text-4xl text-green-dark">
               September 9th, 2023
             </h2>
-            <p className="mt-5 text-xl text-green-primary">
-              Ceremony, Reception, & After Party
-            </p>
+            <div className="text-center">
+              <p className="mt-5 text-xl text-green-primary">
+                Ceremony
+                <span className="block">5:30 - 6:00</span>
+              </p>
+               <p className="mt-5 text-xl text-green-primary">
+                Cocktail Hour
+                <span className="block">6:00 - 7:00</span>
+              </p>
+              <p className="mt-5 text-xl text-green-primary">
+                Reception
+                <span className="block">7:00 - 10:30</span>
+              </p>
+                <p className="mt-5 text-xl text-green-primary">
+                After Party
+                <span className="block">10:30 - 1:30</span>
+              </p>
+            </div>
           </div>
           <div className="w-full max-w-xs text-green-primary text-center">
             <a href="https://thegeraghty.com/">
@@ -31,12 +47,23 @@ const Page: NextPageWithLayout = () => {
               </h2>
             </a>
             <br />
-            <p>Time - TBD</p>
             <p>Black-Tie Optional</p>
             <p>2520 South Hoyne Ave Chicago, IL 60608</p>
             <br />
+            <Link href="/travel" className="hover:text-green-dark">
+            <p>Transportation</p>
+            </Link>
+            <br />
             <p className="italic">
-              Transportation will be provided to the venue from the hotels
+              Shuttles will be provided round trip from the hotel to the venue throughout the night
+            </p>
+            <br />
+            <p className="italic">
+              We encourage guests to utilize Ride Share options or the shuttle bus service
+            </p>
+            <br />
+            <p className="italic">
+              Valet will also be available at the venue
             </p>
           </div>
         </div>
